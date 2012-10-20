785d4bdc5ba59a8f802543f69053ecabb47ace11
Valeriy Vasin <vv@anahoret.com>
2012-10-20 17:48:13 +0400

    Insert copy task button into the menu panel.

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,1 +1,27 @@
 // here will be content script of the plugin
+
+/**
+ * @todo Research, how we could normally insert our group into the page
+ */
+
+function createCopyButton() {
+    var copyButton = $('<a />', {
+            href: '#',
+            title: 'Copy task title into the clipboard',
+            html: '<span class="icon copy-icon"></span><span class="trigger-text"> Copy Task Title</span>',
+            'class': 'toolbar-trigger'
+        }),
+        toolbarItem = $('<li />', { 'class': 'toolbar-item toolbar-item-copy' }).append(copyButton),
+        toolbarGroup = $('<ul />', { 'class': 'toolbar-group' }).append(toolbarItem);
+
+    copyButton.on('click', function (e) {
+        // handler for the click
+        e.preventDefault();
+    });
+
+    $('#opsbar-opsbar-transitions').after(toolbarGroup);
+}
+
+// contents scripts are inserted after DOM is ready
+// that's why we could use it right there
+createCopyButton();
